ac96981c78bba6a263ce67587ebc623997387ca9
bit4woo <bit4woo@163.com>
2020-03-27 16:33:50 +0800

    Update start.sh

--- build/start.sh
+++ build/start.sh
@@ -2,7 +2,7 @@
 cd /blog/code2sec.com
 git pull
 cd ..
+cp /blog/code2sec.com/build/pelicanconf.py /blog/pelicanconf.py
 pelican code2sec.com
 service nginx start
 tail -f /var/log/nginx/error.log
-cp /blog/code2sec.com/cors.html /blog/output/
